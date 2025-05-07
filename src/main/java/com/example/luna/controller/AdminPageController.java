7c8b471cf1453fec91803ca636630c89ad22da1f
smsroql12 <smsroql12@naver.com>
2025-05-07 23:19:04 +0900

    view page 작업

--- src/main/java/com/example/luna/controller/AdminPageController.java
+++ src/main/java/com/example/luna/controller/AdminPageController.java
@@ -1,4 +1,16 @@
 package com.example.luna.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminPageController {
+    @GetMapping("/index")
+    public String adminPage() {
+        return "admin/index";
+    }
 }
--- src/main/java/com/example/luna/controller/MainController.java
+++ src/main/java/com/example/luna/controller/MainController.java
@@ -1,16 +1,37 @@
 package com.example.luna.controller;
 
-import jakarta.servlet.http.HttpSession;
+import com.example.luna.entity.Product;
+import com.example.luna.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     @GetMapping("/")
-    public String getMain(HttpSession session, Model model) {
+    public String getMain() {
         return "index";
+    }
+
+    private final ProductRepository productRepository;
+
+
+    @GetMapping("/view")
+    public String viewProduct(@RequestParam("id") Long id, Model model) {
+        Optional<Product> productOpt = productRepository.findById(id);
+
+        if (productOpt.isPresent()) {
+            model.addAttribute("product", productOpt.get());
+            return "view";
+        } else {
+            model.addAttribute("message", "해당하는 상품 정보가 없습니다.");
+            model.addAttribute("link", "main");
+            return "message"; // 성공 안내 페이지
+        }
     }
 }
